e498309ae21ff0c3dd5f5349046b4cd5595d70c0
fackloar <fackloar@gmail.com>
2022-09-30 16:10:19 +0300

    SOAP, ASMX, SopaUI

---
+++ LibraryService/LibraryService.Web/Models/SearchType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LibraryService.Web.Models
+{
+    public enum SearchType
+    {
+        [Display(Name = "Заголовок")]
+        Title,
+        [Display(Name = "Автор")]
+        Author,
+        [Display(Name = "Категория")]
+        Category
+    }
+}
---
+++ LibraryService/LibraryService/LibraryWebService.asmx.cs
@@ -0,0 +1,54 @@
+﻿using LibraryService.Models;
+using LibraryService.Services;
+using LibraryService.Services.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace LibraryService
+{
+    /// <summary>
+    /// Summary description for LibraryWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class LibraryWebService : System.Web.Services.WebService
+    {
+        
+        #region Services
+
+        private readonly ILibraryRepositoryService _libraryRepositoryService;
+
+        #endregion
+
+        public LibraryWebService()
+        {
+            _libraryRepositoryService = new LibraryRepository(new LibraryDatabaseContext());
+        }
+
+        [WebMethod]
+        public Book[] GetBooksByTitle(string title)
+        {
+            return _libraryRepositoryService.GetByTitle(title).ToArray();
+        }
+
+        [WebMethod]
+        public Models.Book[] GetBooksByAuthor(string authorName)
+        {
+            return _libraryRepositoryService.GetByAuthor(authorName).ToArray();
+        }
+
+        [WebMethod]
+        public Models.Book[] GetBooksByCategory(string category)
+        {
+            return _libraryRepositoryService.GetByCategory(category).ToArray();
+        }
+
+
+    }
+}
