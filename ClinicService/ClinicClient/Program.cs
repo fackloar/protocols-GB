4591f08f4c615cc7485845a76c6958b0bfd0b41f
fackloar <fackloar@gmail.com>
2022-10-11 15:11:02 +0300

    Сервис аутентификации

--- ClinicService/ClinicClient/Program.cs
+++ ClinicService/ClinicClient/Program.cs
@@ -1,4 +1,7 @@
-﻿using Grpc.Net.Client;
+﻿using ClinicServiceProtos;
+using Grpc.Core;
+using Grpc.Net.Client;
+using static ClinicServiceProtos.AuthenticateService;
 using static ClinicServiceProtos.ClinicClientService;
 
 namespace ClinicClient
@@ -7,12 +10,43 @@ namespace ClinicClient
     {
         static void Main(string[] args)
         {
-            AppContext.SetSwitch(
-                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            //AppContext.SetSwitch(
+            //    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
 
 
-            ClinicClientServiceClient client = new ClinicClientServiceClient(channel);
+            AuthenticateServiceClient authenticateServiceClient = new AuthenticateServiceClient(channel);
+
+
+            var authenticationResponse = authenticateServiceClient.Login(new AuthenticationRequest
+            {
+                UserName = "sample@gmail.com",
+                Password = "12345"
+            });
+
+            if (authenticationResponse.Status != 0)
+            {
+                Console.WriteLine("Authentication error.");
+                Console.ReadKey();
+                return;
+            }
+
+
+            Console.WriteLine($"Session token: {authenticationResponse.SessionContext.SessionToken}");
+
+            var callCredentials = CallCredentials.FromInterceptor((c, m) =>
+            {
+                m.Add("Authorization",
+                    $"Bearer {authenticationResponse.SessionContext.SessionToken}");
+                return Task.CompletedTask;
+            });
+
+            var protectedChannel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions
+            {
+                Credentials = ChannelCredentials.Create(new SslCredentials(), callCredentials)
+            });
+
+            ClinicClientServiceClient client = new ClinicClientServiceClient(protectedChannel);
 
             var createClientResponse = client.CreateClient(new ClinicServiceProtos.CreateClientRequest
             {
